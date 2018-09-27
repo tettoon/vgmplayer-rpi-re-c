17ff83e619c1015e9b9004f714e123d34346f31c
Tettoon <tettoon@gmail.com>
2018-09-27 16:48:03 +0900

    Remove printf.

--- s98.c
+++ s98.c
@@ -75,8 +75,6 @@ void s98_load(void *buf, size_t bufsz, s98_t *s98) {
 
     s98->frequency = (uint32_t) (1.0 / (timer_info / timer_info2));
 
-    printf("timer_info=%f, timer_info2=%f, frequency=%d\n", timer_info, timer_info2, s98->frequency);
-
     _s98_seterr(s98, S98_OK);
 }
 
